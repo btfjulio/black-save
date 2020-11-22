5a1e01e5c10934a475f30bb5ed9eb1a6499af5ed
Bruno Tostes <btfjulio@hotmail.com>
2020-11-22 19:08:28 -0300

    add changes to offers

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,5 +1,10 @@
 class OffersController < ApplicationController
   skip_before_action :authenticate_user!
   def index 
+    @offers = Offer.all
+  end
+
+  def show
+    @offer = Offer.find(params[:id])
   end
 end
