fb51cb24c24f647950ea471fd2ddae49130cfcf4
Bruno Tostes <btfjulio@hotmail.com>
2020-11-23 10:13:15 -0300

    add grid styles and seeds to app

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,45 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'mechanize'
+
+class ProductUpdater
+  def initialize(_options = {})
+    @page = 1
+    @agent = Mechanize.new
+    @token = ENV['API_SAVEWHEY_TOKEN']
+    @user = ENV['API_SAVEWHEY_USER']
+  end
+
+  def access_api
+    products = JSON.parse(make_request.body)
+    products.each { |product| save_product(product) }
+    puts "All products imported"
+  end
+
+  def save_product(product)
+    # check if already exists on DB
+    product.merge!(parse_product(product))
+    db_product = Product.where(api_code: product['id']).first
+    if db_product.nil?
+      product.except!('id', 'brand', 'category', 'subcategory')
+      p = Product.create!(product)
+      "#{p.name} - #{p.weight} - #{p.flavor} "
+    else
+      puts "#{db_product.name} - #{db_product.weight} - #{db_product.flavor} already on DB"
+    end
+  end
+
+  def make_request
+    api_endpoint = "https://savewhey-api.herokuapp.com/api/v2/base_suplements?page=#{@page}&#{ENV['API_SAVEWHEY_USER']}&#{ENV['API_SAVEWHEY_TOKEN']}"
+    @agent.get(api_endpoint)
+  rescue StandardError => e
+    puts e
+  end
+
+  def parse_product(product)
+    {
+      'category_id': Category.where(name: product['category']).first&.id,
+      'subcategory_id': Subcategory.where(name: product['subcategory']).first&.id,
+      'brand_id': Brand.where(search_name: product['brand']['search_name']).first&.id,
+      'api_code': product['id']
+    }
+  end
+end
