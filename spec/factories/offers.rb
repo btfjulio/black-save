afb302531249c3d3f6764300d496b88562df5371
Bruno Tostes <btfjulio@hotmail.com>
2020-12-09 17:17:58 -0300

    add offers factory

---
+++ spec/factories/offers.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :offer do
+    sequence(:name) { |n| "Basic #{n}" }
+    cupom { %i(SAVEWHEY SAVEWHEY11 SUPLE30 SAVE-BLACK).sample }
+    description { Faker::Lorem.paragraph }
+    price { Faker::Commerce.price(range: 100.0..400.0)}
+  end
+end
+
