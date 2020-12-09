7094bd9dd64f7afb3a630a1668d0861ce580a7d2
Bruno Tostes <btfjulio@hotmail.com>
2020-12-09 17:25:00 -0300

    add tests to swapi service

--- spec/factories/offers.rb
+++ spec/factories/offers.rb
@@ -4,6 +4,7 @@ FactoryBot.define do
     cupom { %i(SAVEWHEY SAVEWHEY11 SUPLE30 SAVE-BLACK).sample }
     description { Faker::Lorem.paragraph }
     price { Faker::Commerce.price(range: 100.0..400.0)}
+    store
   end
 end
 
